a5980627dab6f58e34315bb0e1522d2d566a968f
asmaasif396@gmail.com <asmaasif396@gmail.com>
2024-06-23 18:32:13 +0500

    added chat gateway: implemented web sockets for real time chat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { AppGateway } from './app.gateway';
 import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
+  controllers: [AppController],
+  providers: [AppService, AppGateway],
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.CHAT_DB)
+    MongooseModule.forRoot(process.env.CHAT_DB),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
