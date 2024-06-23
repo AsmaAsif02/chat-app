222d6d8ab30c1c471aaa4dd8d35ecf1a2dfa0efd
asmaasif396@gmail.com <asmaasif396@gmail.com>
2024-06-24 00:15:59 +0500

    Updated WebSocket gateway to target specific users. Implemented user-to-socket mapping for precise message delivery.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,21 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AppGateway } from './app.gateway';
+import { MessagesGateway } from './app.gateway';
+import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { AuthModule } from './auth/auth.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService, AppGateway],
+  providers: [AppService, MessagesGateway],
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.CHAT_DB),
+    UsersModule,
+    AuthModule,
+    MessagesModule,
   ],
 })
 export class AppModule {}
